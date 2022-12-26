6f1bf82c7fc749184472ce125e6cc9e1657112a3
wingcomstriker405 <wingcomstriker@gmail.com>
2022-12-26 15:39:46 +0100

    add optimization

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import cg.common.Gate;
 import cg.common.LogicComponent;
-import cg.component.CarryLookAheadAdder;
-import cg.component.FullAdder;
-import cg.component.Register;
-import cg.component.RippleCarryAdder;
+import cg.component.*;
+import cg.optimization.PassThroughOptimizer;
 import cg.synthesis.Circuit;
 import cg.synthesis.SynthesisContext;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Main
 {
@@ -19,10 +19,16 @@ public class Main
     {
         // this path can be substituted for an actual blueprint file so the creation can be built directly
         Path of = Path.of("blueprint.json");
-//        LogicComponent component = new CarryLookAheadAdder("my adder", 32);
+        LogicComponent component = new CarryLookAheadAdder("my adder", 120);
 //        LogicComponent component = new FullAdder("my adder");
+//        LogicComponent component = new Register("my register", 64);
+//        LogicComponent component = new Selector("my selector", 4);
+//        LogicComponent component = new Multiplexer("my selector", 8, 16, 4);
+//        LogicComponent component = new Memory("my memory", 16, 128, 8);
+//        LogicComponent component = new Selection("my selector", 16, 8);
+//        LogicComponent component = new Counter("counter", 8);
+//        LogicComponent component = new RippleCarryAdder("rc", 16);
 
-        LogicComponent component = new Register("my register", 64);
         String build = build(component);
         Files.writeString(of, build.replaceAll("[ \r\t\n]", ""));
     }
@@ -31,6 +37,12 @@ public class Main
     {
         SynthesisContext context = new SynthesisContext();
         Circuit synthesise = component.synthesise(context);
+
+        Set<Integer> io = new HashSet<>();
+        synthesise.inputs().forEach((a, b) -> b.forEach(c -> io.add(c.id())));
+        synthesise.outputs().forEach((a, b) -> b.forEach(c -> io.add(c.id())));
+
+        PassThroughOptimizer.check(io, synthesise.gates());
 
         getRandomizedColor(synthesise.inputs());
         getRandomizedColor(synthesise.outputs());
---
+++ src/cg/optimization/PassThroughOptimizer.java
@@ -0,0 +1,87 @@
+package cg.optimization;
+
+import cg.common.Gate;
+import cg.common.Operation;
+
+import java.util.*;
+
+public class PassThroughOptimizer
+{
+    public static void check(Set<Integer> io, List<Gate> gates)
+    {
+        gates.sort(Comparator.comparingInt(Gate::id));
+
+        List<List<Gate>> in = new ArrayList<>();
+        List<List<Gate>> out = new ArrayList<>();
+
+        // prepare the input lists
+        for (int i = 0; i < gates.size(); i++)
+        {
+            in.add(new ArrayList<>());
+            out.add(new ArrayList<>());
+        }
+
+        // compute the inputs of the gates
+        for (Gate g : gates)
+        {
+            List<Integer> outputs = g.outputs();
+            for (Integer output : outputs)
+            {
+                in.get(gates.get(output).id()).add(g);
+            }
+            out.get(g.id()).addAll(outputs.stream().map(gates::get).toList());
+        }
+
+        Set<Operation> neutral = Set.of(
+                Operation.AND,
+                Operation.OR,
+                Operation.XOR
+        );
+
+        Set<Integer> removed = new HashSet<>();
+        int optimize = 0;
+        boolean changed;
+        do
+        {
+
+            changed = false;
+
+            for (int i = 0; i < gates.size(); i++)
+            {
+                Gate current = gates.get(i);
+                if(removed.contains(current.id()) || io.contains(current.id())) continue;
+
+                if (in.get(current.id()).size() == 1 && !out.get(current.id()).isEmpty() && neutral.contains(current.op()))
+                {
+                    // change the gate itself
+                    Gate gate = in.get(current.id()).get(0);
+                    // remove the current gate
+                    gate.outputs().removeIf(k -> k == current.id());
+                    // add the outputs
+                    gate.outputs().addAll(current.outputs());
+
+                    // update the in and out lists
+
+                    // - remove the current gate
+                    for (Integer output : current.outputs())
+                        in.get(output).remove(current);
+                    out.get(gate.id()).remove(current);
+
+                    // - add the previous gate to the current outputs
+                    for (Integer output : current.outputs())
+                        in.get(output).add(gate);
+                    out.get(gate.id()).addAll(current.outputs().stream().map(gates::get).toList());
+
+                    removed.add(current.id());
+                    optimize++;
+                    changed = true;
+                }
+            }
+        }
+        while (changed);
+
+        gates.removeIf(g -> removed.contains(g.id()));
+
+        System.out.println("GATES TO OPTIMIZE: " + optimize);
+    }
+}
