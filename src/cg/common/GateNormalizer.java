1d09fcce08bc849a359fd10b342c677005b99317
wingcomstriker405 <wingcomstriker@gmail.com>
2022-12-28 11:29:18 +0100

    add docs to GateNormalizer

--- src/cg/common/GateNormalizer.java
+++ src/cg/common/GateNormalizer.java
@@ -2,8 +2,19 @@ package cg.common;
 
 import java.util.List;
 
+/**
+ * Utility class to move all gates in order to have the corner start in the origin.
+ */
 public class GateNormalizer
 {
+    private GateNormalizer()
+    {
+    }
+
+    /**
+     * Normalizes all gates to let the corner be located at (0, 0, 0).
+     * @param gates the gates to normalize
+     */
     public static void normalize(List<Gate> gates)
     {
         BoundingBox box = BoundingBox.of(gates);
