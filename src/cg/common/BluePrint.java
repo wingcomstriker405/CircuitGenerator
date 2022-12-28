ad1d4b88702b245f07efaaf15cc28ea5e4769bc8
wingcomstriker405 <wingcomstriker@gmail.com>
2022-12-28 10:51:20 +0100

    add docs to BluePrint

--- src/cg/common/BluePrint.java
+++ src/cg/common/BluePrint.java
@@ -2,8 +2,14 @@ package cg.common;
 
 import java.util.List;
 
+/**
+ * Represents a blueprint used by the {@link BluePrintComponent}.
+ */
 public record BluePrint(List<Gate> gates)
 {
+    /**
+     * Normalizes the blueprint. The origin of the blueprint will be at (0, 0, 0) to make positioning easier.
+     */
     public void normalize()
     {
         GateNormalizer.normalize(this.gates);
