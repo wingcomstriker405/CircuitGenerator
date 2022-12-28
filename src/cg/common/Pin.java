e3ce69ce8bdd92d458c1d24dc1f53008cf76f846
wingcomstriker405 <wingcomstriker@gmail.com>
2022-12-28 11:35:03 +0100

    add docs to Pin

--- src/cg/common/Pin.java
+++ src/cg/common/Pin.java
@@ -1,15 +1,26 @@
 package cg.common;
 
+/**
+ * Extension of the {@link Vec} to associate a colored gate in a blueprint with the vector.
+ */
 public class Pin extends Vec
 {
     private final String color;
 
+    /**
+     * @param name the name of the pin
+     * @param color the color of the pin
+     */
     public Pin(String name, String color)
     {
         super(name, 1);
         this.color = color;
     }
 
+    /**
+     * Returns the color of the pin.
+     * @return the color
+     */
     public String getColor()
     {
         return this.color;
