097b141fe01366e590b2ebe0834e6895932bd54c
wingcomstriker405 <wingcomstriker@gmail.com>
2022-12-28 10:38:43 +0100

    add documentation to Assert

--- src/cg/common/Assert.java
+++ src/cg/common/Assert.java
@@ -2,6 +2,13 @@ package cg.common;
 
 public class Assert
 {
+    private Assert() { }
+
+    /**
+     * Throws a {@link RuntimeException} if check is false.
+     * @param check indicates if the assertion was successful
+     * @param message the error message if check did not succeed
+     */
     public static void assertThat(boolean check, String message)
     {
         if (!check)
