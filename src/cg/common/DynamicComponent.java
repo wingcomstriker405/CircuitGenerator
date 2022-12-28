ff2fded750d6d87c3bc1226d7747794cecd25357
wingcomstriker405 <wingcomstriker@gmail.com>
2022-12-28 11:23:53 +0100

    add docs to DynamicComponent

--- src/cg/common/DynamicComponent.java
+++ src/cg/common/DynamicComponent.java
@@ -6,6 +6,10 @@ import cg.synthesis.SynthesisContext;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * A component with fine-grained control over the connection between individual gates.
+ * This component type is a hybrid between {@link ComplexComponent} and {@link BluePrintComponent}.
+ */
 public abstract class DynamicComponent extends LogicComponent
 {
 
@@ -19,6 +23,11 @@ public abstract class DynamicComponent extends LogicComponent
     private final Map<String, Operation> names = new HashMap<>();
     private final Map<String, LogicComponent> components = new HashMap<>();
 
+    /**
+     * @param name the name of the component
+     * @param inputs the inputs
+     * @param outputs the outputs
+     */
     protected DynamicComponent(String name, List<Vec> inputs, List<Vec> outputs)
     {
         super(name);
@@ -26,42 +35,87 @@ public abstract class DynamicComponent extends LogicComponent
         this.out = new GateWay(">", outputs);
     }
 
+    /**
+     * Adds a components to this component.
+     * The inputs and outputs of that component will be exposed by adding the name of the component as prefix separated
+     * by an underscore ('_') to the input and output names (e.g. comp_inp2).
+     * @param component the component to add
+     */
     protected void add(LogicComponent component)
     {
         this.components.put(component.getName(), component);
     }
+
+    /**
+     * Adds a new gate to this component
+     * @param name the name of the gate
+     * @param operation the operation of the gate
+     * @see Operation
+     */
     protected void add(String name, Operation operation)
     {
         this.names.put(name, operation);
     }
 
+    /**
+     * Adds a new gate to this component
+     * @param name the name of the gate
+     * @param index an index that is appended to the name
+     * @param operation the operation of the gate
+     * @see Operation
+     */
     protected void add(String name, int index, Operation operation)
     {
         this.names.put(name + index, operation);
     }
 
+    /**
+     * Connects two gates.
+     * @param start the source gate
+     * @param end the destination gate
+     */
     protected void connect(String start, String end)
     {
         this.connections.add(new Connection(start, end));
     }
 
+    /**
+     * Adds a components to this component
+     * @param start the source gate
+     * @param startIndex the appended index
+     * @param end the destination gate
+     * @param endIndex the appended index
+     */
     protected void connect(String start, int startIndex, String end, int endIndex)
     {
         this.connections.add(new Connection(start + startIndex, end + endIndex));
     }
 
+    /**
+     * Returns the outputs that the component has.
+     * @return the outputs
+     */
     @Override
     public List<Vec> getOutputs()
     {
         return this.out.getOutputs();
     }
 
+    /**
+     * Returns the inputs that the component has.
+     * @return the inputs
+     */
     @Override
     public List<Vec> getInputs()
     {
         return this.in.getInputs();
     }
 
+    /**
+     * Synthesizes an instance of the component. This generates the gates and applies all the connections.
+     * @param context the synthesis context
+     * @return the circuit
+     */
     @Override
     public Circuit synthesise(SynthesisContext context)
     {
@@ -106,6 +160,12 @@ public abstract class DynamicComponent extends LogicComponent
         return circuit;
     }
 
+    /**
+     * A callback to position the components.
+     * @param circuit the synthesized circuit
+     * @param mapping the map of all available gates
+     * @param circuits the contained circuits
+     */
     protected abstract void layout(Circuit circuit, Map<String, Gate> mapping, Map<String, Circuit> circuits);
 
     private static Map<String, List<Gate>> fromGateWay(SynthesisContext context, GateWay way)
